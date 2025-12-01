9ee27404b92126769c463710fee00d97faeaf813
Sivatho Conjwa <SConjwa@clientele.co.za>
2025-12-01 09:30:18 +0200

    Added new module for Validate Bank Account Request

---
+++ ClientServicing/Main/Models/Bank/ValidateBankAccountRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.Bank
+{
+    public class ValidateBankAccountRequest
+    {
+        public string idNumber { get; set; }
+        public string bankAccountHolderInitials { get; set; }
+        public string surname { get; set; }
+        public string bankName { get; set; }
+        public string bankAccountNumber { get; set; }
+        public string bankBranchCode { get; set; }
+        public string bankAccountType { get; set; }
+    }
+}

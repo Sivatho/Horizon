c0070202cf406982ab9109f716ef2508a2bb5870
Sivatho Conjwa <SConjwa@clientele.co.za>
2025-12-04 17:47:49 +0200

    Added addition models that as us a property types by CorrectBankAccountType in ValidateBankAccountQAVSRResponse and GSD in ValidateBankAccountQAVSRRequest

---
+++ ClientServicing/Main/Models/General/CorrectBankAccountType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.General
+{
+    public class CorrectBankAccountType
+    {
+        public int type { get; set; }
+    }
+}

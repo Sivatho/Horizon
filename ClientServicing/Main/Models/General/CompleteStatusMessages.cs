14133811d3bdf3ee0b2d0afc59dcad6f25d5c1eb
Sivatho Conjwa <SConjwa@clientele.co.za>
2025-12-09 10:06:49 +0200

    Added a new model for CompleteStatusMessages

---
+++ ClientServicing/Main/Models/General/CompleteStatusMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.General
+{
+    public class CompleteStatusMessages
+    {
+        public bool proCompleted { get; set; }
+        public bool success { get; set; }
+        public string message { get; set; }
+    }
+}

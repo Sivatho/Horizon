0bd1cfad30259d852f89ff4cff8c7dbc91ae6d1b
Sivatho Conjwa <SConjwa@clientele.co.za>
2025-12-09 10:06:00 +0200

    Added a new model for CanChangeBankAccount Response

---
+++ ClientServicing/Main/Models/Bank/CanChangeBankAccountResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientServicing.Main.Models.General;
+
+namespace ClientServicing.Main.Models.Bank
+{
+    public class CanChangeBankAccountResponse
+    {
+        public bool succeeded { get; set; }
+        public string message { get; set; }
+        public string errors { get; set; }
+        public CompleteStatusMessages data { get; set; }
+    }
+}

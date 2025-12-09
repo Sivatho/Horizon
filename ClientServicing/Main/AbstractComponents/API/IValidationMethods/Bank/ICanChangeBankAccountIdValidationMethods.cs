3e5300be1129deed259ca4be5df83d09288ea6cd
Sivatho Conjwa <SConjwa@clientele.co.za>
2025-12-09 10:49:37 +0200

    Added ICanChangeBankAccountIdValidationMethods interface with all the pre-requisite methods declared

---
+++ ClientServicing/Main/AbstractComponents/API/IValidationMethods/Bank/ICanChangeBankAccountIdValidationMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientServicing.Main.AbstractComponents.API.ValidationMethods.Bank;
+using ClientServicing.Main.Models.Bank;
+
+namespace ClientServicing.Main.AbstractComponents.API.IValidationMethods.Bank
+{
+    public interface ICanChangeBankAccountIdValidationMethods
+    {
+        public void ValidateResponseIsNotNullOrEmpty(CanChangeBankAccountResponse canChangeBankAccountResponse);
+        public void ValidateResponseIsNullOrWhiteSpace(CanChangeBankAccountResponse canChangeBankAccountResponse);
+        public void ValidateCanChangeBankAccountResponseDataIsNotNullOrEmpty(CanChangeBankAccountResponse canChangeBankAccountResponse);
+    }
+}

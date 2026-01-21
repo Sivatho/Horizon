099160b005ee7eda9d261f5589825da7d70d18d5
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-01-21 14:16:37 +0200

    Added ChangePolicyDocRequest model and json data

---
+++ ClientServicing/Main/Models/Policy/ChangePolicyDocRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.Policy
+{
+    public class ChangePolicyDocRequest
+    {
+        public string policyNo { get; set; }
+        public string currentdocdate { get; set; }
+        public string changetype { get; set; }
+        public string newdocdate { get; set; }
+        public int newdebitdate { get; set; }
+        public string comment { get; set; }
+    }
+}

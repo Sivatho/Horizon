db4cd9ad8ee9f64f85d950d04f869d9fc4b38050
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-01-28 10:55:18 +0200

    Added a new model for check waiting period Requests

---
+++ ClientServicing/Main/Models/Policy/CheckWaitingPeriodRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.Policy
+{
+    public class CheckWaitingPeriodRequest
+    {
+        public int PolicyNumber { get; set; }
+        public int claimCategoryCd { get; set; }
+        public int insuredLifeEntityNo { get; set; }
+    }
+}

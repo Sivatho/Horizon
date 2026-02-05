1d0e47e1209b995058e7ad76a8427c3970c1dff0
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-05 15:47:31 +0200

    Created API Test class for GetPayerDetailsByPolicyNumber  test builders a request  executes and returns a jsonresponse

---
+++ ClientServicing/Test/Tests/API/TDD/Payer/GetPayerDetailsByPolicyNumberAPITests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ClientServicing.Main.Controller;
+using ClientServicing.Main.Models.General;
+using ClientServicing.Main.Resources.Helper;
+
+namespace ClientServicing.Test.Tests.API.TDD.Payer
+{
+    public class GetPayerDetailsByPolicyNumberAPITests
+    {
+        PayerAPIClient payerAPIClient = new();
+        UtilitiesHelper utilitiesHelper = new();
+
+        [Test]
+        public async Task Given_GetPayerDetailsByPolicyNumberRequest_When_GetPayerDetailsByPolicyNumberAsync_Then() {
+            var getPayerDetailsByPolicyNumberRequest = JsonSerializer.Deserialize<PolicyNoAndEffectiveDate>(
+                utilitiesHelper.ReadTestDataJson("Payer/Data", "GetPayerDetailsByPolicyNumberPayloadRequestIsValid.json"));
+            getPayerDetailsByPolicyNumberRequest.auditToken = Guid.NewGuid().ToString();
+
+            var response = await payerAPIClient.GetPayerDetailsByPolicyNumberAsync(getPayerDetailsByPolicyNumberRequest);
+
+        }
+    }
+}

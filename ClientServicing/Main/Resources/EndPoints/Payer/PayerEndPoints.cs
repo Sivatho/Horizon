c976a7d14a83cd397df9a51c613f41b17be471c6
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-05 13:22:37 +0200

    Added PayerEndPoints - a simple utility responsible for centralizing and resolving API endpoint URLs

---
+++ ClientServicing/Main/Resources/EndPoints/Payer/PayerEndPoints.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Resources.EndPoints.Payer
+{
+    public class PayerEndPoints
+    {
+        public enum EndPoints
+        {
+            GetPayerDetailsByPolicyNumber,
+            UpsertBankingAndPayer
+        }
+        public static string GetEndPoint(EndPoints endPoints)
+        {
+            return endPoints switch
+            {
+                EndPoints.GetPayerDetailsByPolicyNumber => "/api/Payer/GetPayerDetailsByPolicyNumber",
+                EndPoints.UpsertBankingAndPayer         => "/api/Payer/UpsertBankingAndPayer",
+                _ => throw new ArgumentOutOfRangeException(nameof(endPoints), endPoints, null)
+            };
+        }
+    }
+}

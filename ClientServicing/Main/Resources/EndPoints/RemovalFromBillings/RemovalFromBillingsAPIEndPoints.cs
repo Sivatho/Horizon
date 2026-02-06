953c2deb6605505b25b4ac0e611c64605ae35ad2
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-06 15:44:34 +0200

    Created  endpoints for RemovalFromBillings

---
+++ ClientServicing/Main/Resources/EndPoints/RemovalFromBillings/RemovalFromBillingsAPIEndPoints.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Resources.EndPoints.RemovalFromBillings
+{
+    public class RemovalFromBillingsAPIEndPoints
+    {
+        public enum EndPoints
+        {
+            RemovalFromBillingsHistory,
+            CancelRemovalFromBillings,
+            RemoveFromBillings
+        }
+        public static string GetEndPoint(EndPoints endPoints)
+        {
+            return endPoints switch
+            {
+                EndPoints.RemovalFromBillingsHistory => "/api/RemovalFromBillings/RemovalFromBillingsHistory",
+                EndPoints.CancelRemovalFromBillings =>  "/api/RemovalFromBillings/CancelRemovalFromBillings",
+                EndPoints.RemoveFromBillings =>         "/api/RemovalFromBillings/RemoveFromBillings",
+                _ => throw new ArgumentOutOfRangeException(nameof(endPoints), endPoints, null)
+            };
+        }
+    }
+}

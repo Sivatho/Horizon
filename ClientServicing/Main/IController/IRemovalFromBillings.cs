a74e7c389ba15b1547c0f11001c243bc2bb487fb
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-06 15:43:49 +0200

    Created a new  Interface

---
+++ ClientServicing/Main/IController/IRemovalFromBillings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ClientServicing.Main.IController
+{
+    public interface IRemovalFromBillings
+    {
+        Task<RestResponse> RemovalFromBillingsHistoryAsync<T>(T payload) where T : class;
+        Task<RestResponse> CancelRemovalFromBillingsAsync<T>(T payload) where T : class;
+        Task<RestResponse> RemoveFromBillingsAsync<T>(T payload) where T : class;
+    }
+}

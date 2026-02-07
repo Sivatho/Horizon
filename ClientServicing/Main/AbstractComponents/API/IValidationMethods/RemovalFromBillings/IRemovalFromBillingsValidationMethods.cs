4ad4ed3603492d6531b18c10c54c47f1e4ef0474
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-07 18:05:11 +0200

    Added Interface that defines Validation Methods

---
+++ ClientServicing/Main/AbstractComponents/API/IValidationMethods/RemovalFromBillings/IRemovalFromBillingsValidationMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientServicing.Main.Models.RemovalFromBillings;
+
+namespace ClientServicing.Main.AbstractComponents.API.IValidationMethods.RemovalFromBillings
+{
+    public interface IRemovalFromBillingsValidationMethods
+    {
+        public void ValidateRemovalFromBillingsRequestIsNotNullOrEmpty_And_IntegerIsNotLessThanZero_And_DateTimeIsNotEqualToDefault(RemovalFromBillingsRequest removalFromBillingsRequest);
+        public void ValidateRemovalFromBillingsRespondeIsNotNullOrEmpty_And_IntegerIsNotLessThanZero_And_DateTimeIsNotEqualToDefault(RemovalFromBillingsHistoryResponse removalFromBillingsHistoryResponse);
+    }
+}

8920a3924a9baf1769db4f6e9616549a74fede2a
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-09 08:27:18 +0200

    Added RemoveFromBilling API Tests

---
+++ ClientServicing/Test/Tests/API/TDD/RemovalFromBillings/RemoveFromBillingsAPITests.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Text.Json;
+using ClientServicing.Main.AbstractComponents.API.ValidationMethods.JsonValidation;
+using ClientServicing.Main.AbstractComponents.API.ValidationMethods.RemovalFromBillings;
+using ClientServicing.Main.Controller;
+using ClientServicing.Main.Models.RemovalFromBillings;
+using ClientServicing.Main.Resources.Helper;
+
+namespace ClientServicing.Test.Tests.API.TDD.RemovalFromBillings
+{
+    [TestFixture]
+    public class RemoveFromBillingsAPITests : RemovalFromBillingsValidationMethods
+    {
+        [Test]
+        public async Task Given_CancelRemovalFromBillingsRequestPayloadIsValid_When_RemoveFromBillingsAsync_Then_ValidateRespondeStatusIsOK_AND_ResponseDataIsNotNullOrEmpty()
+        {
+            RemovalFromBillingsAPIClient removalFromBillingsAPIClient = new();
+            UtilitiesHelper utilitiesHelper = new();
+
+            //Arrange
+            var request = JsonSerializer.Deserialize<RemovalFromBillingsRequest>(utilitiesHelper.ReadTestDataJson("RemovalFromBillings/Data", "RemovalFromBillingsRequestPayloadIsValid.json"));
+            ValidateRemovalFromBillingsRequestIsNotNullOrEmpty_And_IntegerIsNotLessThanZero_And_DateTimeIsNotEqualToDefault(request);
+
+            //Act
+            var response = await removalFromBillingsAPIClient.RemoveFromBillingsAsync(request);
+            var cancelRemovalFromBillingsResponse = PopulateRemovalFromBillingsResponse(response);
+            var schema = ResponseSchemasEnvelope.DataBooleanSchema;
+            //Assert
+            ValidationAssertionHeading();
+            ValidateResponseStatusCode(response, HttpStatusCode.OK);
+            ValidateResponseHeadersAreValid(response);
+            ValidateResponsePropertyNameIsValidAndDataTypesIsValid(response, schema);
+            ValidateResponseShouldMatchSchema(response, schema);
+            ValidateRemovalFromBillingsResponseIsNotNullOrEmpty(cancelRemovalFromBillingsResponse);
+        }
+    }
+}

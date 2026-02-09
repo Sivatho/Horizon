fbe4c95981037693e695cf5067745e12e9d039e4
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-09 10:37:19 +0200

    Created SendTextMessageRequest model

---
+++ ClientServicing/Main/Models/SendPayAtNumber/SendTextMessageRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.SendPayAtNumber
+{
+    public class SendTextMessageRequest
+    {
+        public string? mobileTelephoneNumber { get; set; }
+        public string? subAffiliateCode { get; set; }
+        public string? textMessageContent { get; set; }
+        public VariableDto<string, string>? variables { get; set; }
+        public string? platform { get; set; }
+        public string brand { get; set; }
+        public string? optionalReason { get; set; }
+        public bool optionalBusinessHoursOnly { get; set; }
+        public string? optionalCostCode { get; set; }
+        public string? responseGuid { get; set; }
+    }
+    public class VariableDto<TKey, TValue> {
+        public string? key { get; set; }
+        public string? value { get; set; }
+    }
+}

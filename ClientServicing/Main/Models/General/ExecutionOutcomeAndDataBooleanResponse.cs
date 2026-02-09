79d086fa4a604c2d02bfd9367a73acc1b2d2707d
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-09 08:21:05 +0200

    Added Generic Response for ExecutionOutcome and Data that is of data type bool

---
+++ ClientServicing/Main/Models/General/ExecutionOutcomeAndDataBooleanResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.General
+{
+    public class ExecutionOutcomeAndDataBooleanResponse
+    {
+        public ExecutionOutcome executionOutcome { get; set; }
+        public bool? data { get; set; }
+    }
+}

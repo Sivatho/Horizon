464913b40df2857a92fe39a43137cd0c521736ea
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-09 08:39:21 +0200

    Added class SendPayAtNumberAPIEndPoints. This class is a small helper used to manage API endpoint URLs in a clean and organized way.

---
+++ ClientServicing/Main/Resources/EndPoints/SendPayAtNumber/SendPayAtNumberAPIEndPoints.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Resources.EndPoints.SendPayAtNumber
+{
+    public class SendPayAtNumberAPIEndPoints
+    {
+        public enum EndPoints
+        {
+            send_text_message
+        }
+        public static string GetEndPoint(EndPoints endPoints)
+        {
+            return endPoints switch
+            {
+                EndPoints.send_text_message => "/api/v1.0/SendPayAtNumber/send_text_message",
+                _ => throw new ArgumentOutOfRangeException(nameof(endPoints), endPoints, null)
+            };
+        }
+    }
+}

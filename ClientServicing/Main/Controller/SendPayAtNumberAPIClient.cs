dd24132e559fd8f8e8587e8bb53b0225e3d18b62
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-09 08:59:55 +0200

    Added SendAtNumberAPIClient class containing the implimentation of the methods of the Interface it is dependent on

---
+++ ClientServicing/Main/Controller/SendPayAtNumberAPIClient.cs
@@ -0,0 +1,30 @@
+﻿using ClientServicing.Main.IController;
+using ClientServicing.Main.Resources.EndPoints.SendPayAtNumber;
+using ClientServicing.Main.Resources.Helper;
+using RestSharp;
+using static ClientServicing.Main.Resources.EndPoints.SendPayAtNumber.SendPayAtNumberAPIEndPoints;
+
+namespace ClientServicing.Main.Controller
+{
+    public class SendPayAtNumberAPIClient : ISendPayAtNumber
+    {
+        readonly RestClient restClient;
+        readonly UtilitiesHelper utilitiesHelper = new UtilitiesHelper();
+
+        public async Task<RestResponse> Send_Text_MessageAsync<T>(T payload) where T : class
+        {
+            // Arrange
+            var url = SendPayAtNumberAPIEndPoints.GetEndPoint(EndPoints.send_text_message);
+            Method method = Method.Post;
+            var request = ApiRequestAndResponseHelper.BuildRequest(
+                url,
+                method,
+                payload,
+                out var stopwatch);
+
+            // Act
+            var response = await ApiRequestAndResponseHelper.ExecuteAsync(restClient, request, stopwatch);
+            return response;
+        }
+    }
+}

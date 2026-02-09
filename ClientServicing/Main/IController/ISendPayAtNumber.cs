80d5e130112badef2a0f0e8d815d102e2dee9ec4
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-09 08:38:07 +0200

    Added Interface for SendPayAtNumber which is a contract for expected behavior

---
+++ ClientServicing/Main/IController/ISendPayAtNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ClientServicing.Main.IController
+{
+    public interface ISendPayAtNumber
+    {
+        Task<RestResponse> Send_Text_MessageAsync<T>(T payload) where T : class;
+    }
+}

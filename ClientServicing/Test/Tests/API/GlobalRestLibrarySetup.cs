0d6e3919f20e1d06586fca1a486f117056538a2c
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-12 08:19:43 +0200

    Added GlobalRestLibribraySetp Class this class is responsible for setting up a shared instance of the RestLibrary that can be used across all API tests.

---
+++ ClientServicing/Test/Tests/API/GlobalRestLibrarySetup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientServicing.Main.AbstractComponents.API.Base;
+
+namespace ClientServicing.Test.Tests.API
+{
+    [SetUpFixture]
+    public class GlobalRestLibrarySetup
+    {
+        public static IRestLibrary SharedRestLibrary { get; private set; }
+        
+        [OneTimeSetUp]
+        public void BeforeAllTests()
+        {
+            SharedRestLibrary = new RestLibrary();
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+            SharedRestLibrary.RestClient?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}

ed30903da0855f283d85b3dff2ece60735063263
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-21 01:44:17 +0200

    Added new test method

---
+++ ClientServicing/Test/Tests/API/TDD/AccountHistory/PolicyAcountingHistorySummaryAPITest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ClientServicing.Main.AbstractComponents.API.ValidationMethods.AccountHistory;
+using ClientServicing.Main.AbstractComponents.API.ValidationMethods.JsonValidation;
+using ClientServicing.Main.Controller;
+using ClientServicing.Main.DataAccess.Interface;
+using ClientServicing.Main.Models.AccountHistory;
+using ClientServicing.Main.Resources.Helper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClientServicing.Test.Tests.API.TDD.AccountHistory
+{
+    public class PolicyAcountingHistorySummaryAPITest : PolicyAcountingHistorySummaryValidationMethods
+    {
+        AccountingHistoryAPIClient? accountHistoryAPIClient = null;
+        UtilitiesHelper utilitiesHelper = new();
+        private IDataAccess _dataAccess = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            accountHistoryAPIClient = new AccountingHistoryAPIClient(GlobalTestInfrastructureSetup.SharedRestLibrary);
+            _dataAccess = GlobalTestInfrastructureSetup.ServiceProvider.GetRequiredService<IDataAccess>();
+        }
+        [Test]
+        public async Task Given_PolicyAccountingHistorySummaryIsValid_When_PolicyAccountingHistorySummaryAsync_Then_() {
+            //Arrange
+            var request = JsonSerializer.Deserialize<PolicyAccountHistorySummaryRequest>(
+                utilitiesHelper.ReadTestDataJson
+                ("AccountHistory", "PolicyAccountingHistorySummary.json")
+            );
+            ValidatePolicyAccountHistorySummaryRequestPayload(request);
+            //Act
+            var response = await accountHistoryAPIClient.policyAccountingHistorySummaryAsync<PolicyAccountHistorySummaryRequest>(request);
+            var policyAccountHistorySummaryResponse = PopulatePolicyAccountHistorySummaryResponse(response);
+            var schema = ResponseSchemasEnvelope.policyAccountHistorySummaryResponseSchema;
+            //Assert
+            ValidationAssertionHeading();
+            ValidateResponseStatusCode(response, HttpStatusCode.OK);
+            ValidateResponsePropertyNameIsValidAndDataTypesIsValid(response, schema);
+            ValidateResponseShouldMatchSchema(response, schema);
+            ValidatePolicyAccountHistorySummaryResponsePayload(policyAccountHistorySummaryResponse);
+        }
+    }
+}

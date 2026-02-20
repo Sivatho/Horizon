1faf28d2bc1d84e3a0b0c93436fe452b44c9ffc8
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-21 01:42:23 +0200

    Renamed class parameter name

--- ClientServicing/Main/IController/IAccountHistory.cs
+++ ClientServicing/Main/IController/IAccountHistory.cs
@@ -10,12 +10,12 @@ namespace ClientServicing.Main.IController
 {
     public interface IAccountHistory
     {
-        Task<RestResponse> policyAccountingHistoryAsync<T>(T policyNo) where T : class;
+        Task<RestResponse> policyAccountingHistoryAsync<T>(T payload) where T : class;
         Task<RestResponse> policyAccountingHistorySummaryAsync<T>(T payload) where T : class;
         Task<RestResponse> policyCashReceiptAsync<T>(T payload) where T : class;
         Task<RestResponse> GetStatementLineIDAsync<T>(T payload) where T : class;
-        Task<RestResponse> CashReceiptInfoUpsertAsync(string accountNumber);
-        Task<RestResponse> ManualReceiptInfoUpsertAsync(int policyNo);
+        Task<RestResponse> CashReceiptInfoUpsertAsync<T>(T payload) where T : class;
+        Task<RestResponse> ManualReceiptInfoUpsertAsync<T>(T payload) where T : class;
     }
 }
 

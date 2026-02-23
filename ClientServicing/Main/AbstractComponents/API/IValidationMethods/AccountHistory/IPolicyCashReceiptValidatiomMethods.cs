27af4427364529624e257145790447c342d06a97
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-23 10:18:11 +0200

    Removed unused using statements

--- ClientServicing/Main/AbstractComponents/API/IValidationMethods/AccountHistory/IPolicyCashReceiptValidatiomMethods.cs
+++ ClientServicing/Main/AbstractComponents/API/IValidationMethods/AccountHistory/IPolicyCashReceiptValidatiomMethods.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ClientServicing.Main.Models.AccountHistory;
+﻿using ClientServicing.Main.Models.AccountHistory;
 
 namespace ClientServicing.Main.AbstractComponents.API.IValidationMethods.AccountHistory
 {

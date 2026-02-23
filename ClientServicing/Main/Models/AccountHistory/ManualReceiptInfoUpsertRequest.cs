32a84feb7c5bf1227ad632bd1d7ddb3ddceaa471
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-23 14:18:00 +0200

    Removed unsuade model and added a new model

---
+++ ClientServicing/Main/Models/AccountHistory/ManualReceiptInfoUpsertRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.AccountHistory
+{
+    public class ManualReceiptInfoUpsertRequest
+    {
+        public int policyNo { get; set; }
+        public int transCatCD { get; set; }
+        public DateTime transactionDate { get; set; }
+        public double randAmount { get; set; }
+        public string paymentDescription { get; set; }
+    }
+}

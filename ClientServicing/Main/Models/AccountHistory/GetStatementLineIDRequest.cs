06893a7e2673e81ea0e21cf4c035fef6367f4571
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-23 11:41:09 +0200

    Created a Model for GetStatementLineIDRequest

---
+++ ClientServicing/Main/Models/AccountHistory/GetStatementLineIDRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.AccountHistory
+{
+    public class GetStatementLineIDRequest
+    {
+        public string statementLineID { get; set; }
+        public int policyNo { get; set; }
+        public int transCatCD { get; set; }
+    }
+}

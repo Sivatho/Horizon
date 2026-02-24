b6d309a8e6f5999bc1a99a8688df96bbf48c91d4
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-24 11:02:22 +0200

    Created TriggerEvent Request class and TestData json File

---
+++ ClientServicing/Main/Models/CCMEvent/TriggerEventRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.CCMEvent
+{
+    public class TriggerEventRequest
+    {
+        public int policyNo { get; set; }
+        public string legacyPolicyNo { get; set; }
+        public int partnerCd { get; set; }
+        public int eventTypeCd { get; set; }
+        public string? eventTypeDesc { get; set; }
+        public int quoteId { get; set; }
+        public DateTime effectiveDate { get; set; }
+        public string userId { get; set; }
+    }
+}

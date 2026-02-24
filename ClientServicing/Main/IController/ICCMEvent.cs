928735735097222a0c967bcac43d4ce910c1f3c2
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-24 10:42:14 +0200

    Created interface that contains method signatures only be implemented my classes implementing the interface

---
+++ ClientServicing/Main/IController/ICCMEvent.cs
@@ -0,0 +1,10 @@
+﻿using RestSharp;
+
+namespace ClientServicing.Main.IController
+{
+    public interface ICCMEvent
+    {
+        Task<RestResponse> TriggerEventAsync<T>(T payload) where T : class;
+        Task<RestResponse> GetEventDetailConstructBPEAsync<T>(T payload) where T : class;
+    }
+}

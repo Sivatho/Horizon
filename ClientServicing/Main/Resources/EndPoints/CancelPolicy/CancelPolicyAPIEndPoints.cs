fb26e1944aff3bad98301cf6d7b92a7d09b1181a
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-24 08:23:01 +0200

    Created endpoint for cancelPolicyAPI

---
+++ ClientServicing/Main/Resources/EndPoints/CancelPolicy/CancelPolicyAPIEndPoints.cs
@@ -0,0 +1,18 @@
+﻿namespace ClientServicing.Main.Resources.EndPoints.CancelPolicy
+{
+    public class CancelPolicyAPIEndPoints
+    {
+        public enum EndPoints
+        {
+            UpdateCancelPolicyDetails
+        }
+        public static string GetEndPoint(EndPoints endPoint)
+        {
+            return endPoint switch
+            {
+                EndPoints.UpdateCancelPolicyDetails => "/api/cancelpolicy/update",
+                _ => throw new ArgumentOutOfRangeException(nameof(endPoint), endPoint, null)
+            };
+        }
+    }
+}

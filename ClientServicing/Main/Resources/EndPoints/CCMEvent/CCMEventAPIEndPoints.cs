e69442f794961a68c3189fd784a7221f352b3ef6
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-24 10:50:57 +0200

    Removed unused using statements

--- ClientServicing/Main/Resources/EndPoints/CCMEvent/CCMEventAPIEndPoints.cs
+++ ClientServicing/Main/Resources/EndPoints/CCMEvent/CCMEventAPIEndPoints.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClientServicing.Main.Resources.EndPoints.CCMEvent
+﻿namespace ClientServicing.Main.Resources.EndPoints.CCMEvent
 {
     public class CCMEventAPIEndPoints
     {

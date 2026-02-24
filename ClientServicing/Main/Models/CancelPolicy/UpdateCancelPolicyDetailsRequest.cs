be1c149cd2df785cae5dbfb15a47a0f506548358
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-24 08:24:16 +0200

    Created a model for UpdateCancelPolicyDetails and test data using json file

---
+++ ClientServicing/Main/Models/CancelPolicy/UpdateCancelPolicyDetailsRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.CancelPolicy
+{
+    public class UpdateCancelPolicyDetailsRequest
+    {
+        public int policyNo { get; set; }
+        public int statusCd { get; set; }
+        public int reasonCd { get; set; }
+        public int subReasonCd { get; set; }
+        public DateTime effectiveDate { get; set; }
+        public string comment { get; set; }
+        public string userID { get; set; }
+        public int paymentTypeCD { get; set; }
+        public string providerReference { get; set; }
+    }
+}

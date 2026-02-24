add119dc0f65fdc22b8aaa5172eaa83990d2633a
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-24 10:49:36 +0200

    Created CCMEventAPIEndPoints class which can managed all endpoint that are associated with  CCMEvent APIs

---
+++ ClientServicing/Main/Resources/EndPoints/CCMEvent/CCMEventAPIEndPoints.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Resources.EndPoints.CCMEvent
+{
+    public class CCMEventAPIEndPoints
+    {
+        public enum EndPoint {
+            TriggerEvent, GetEventDetailConstructBPE
+        }
+        public static string GetEndPoints(EndPoint endPoint) {
+            return endPoint switch
+            {
+                EndPoint.TriggerEvent => "api/CCMEvent/TriggerEvent",
+                EndPoint.GetEventDetailConstructBPE => "api/CCMEvent/GetEventDetailConstructBPE"
+            };
+        }
+    }
+}

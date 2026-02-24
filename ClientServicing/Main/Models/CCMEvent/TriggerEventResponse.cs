fec83e9d54d5b8a30ca83c102afbe6a449e8d424
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-24 11:26:00 +0200

    Added trigger event response model

---
+++ ClientServicing/Main/Models/CCMEvent/TriggerEventResponse.cs
@@ -0,0 +1,15 @@
+﻿using ClientServicing.Main.Models.General;
+
+namespace ClientServicing.Main.Models.CCMEvent
+{
+    public class TriggerEventResponse
+    {
+        public ExecutionOutcome executionOutcome { get; set; }
+        public TriggerEvent data { get; set; }
+    }
+    public class TriggerEvent {
+        public string token { get; set; }
+        public bool success { get; set; }
+        public string message { get; set; }
+    }
+}

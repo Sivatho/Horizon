a76857822a371a594a1d494fd8d1c2b89ff805fe
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-24 11:06:32 +0200

    Created GetEventDetailConstructBPE Request class and Test Data json file

---
+++ ClientServicing/Main/Models/CCMEvent/GetEventDetailConstructBPERequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ClientServicing.Main.Models.CCMEvent
+{
+    public class GetEventDetailConstructBPERequest
+    {
+        public int policyNo { get; set; }
+        public int eventTypeCd { get; set; }
+        public DateTime effectiveDate { get; set; }
+    }
+}

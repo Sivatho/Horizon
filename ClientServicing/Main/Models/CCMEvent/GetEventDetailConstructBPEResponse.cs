990df6ff88b92affc034cb06493ec84d27d3cfdb
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-25 08:49:28 +0200

    Added response model for GetEventDetailConstructBPE

---
+++ ClientServicing/Main/Models/CCMEvent/GetEventDetailConstructBPEResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientServicing.Main.Models.General;
+
+namespace ClientServicing.Main.Models.CCMEvent
+{
+    public class GetEventDetailConstructBPEResponse
+    {
+        public ExecutionOutcome executionOutcome { get; set; }
+        public GetEventDetailConstructBPE data { get; set; }
+    }
+
+    public class GetEventDetailConstructBPE
+    {
+        public string jsonData { get; set; }
+        public bool success { get; set; }
+        public string message { get; set; }
+    }
+}
+    

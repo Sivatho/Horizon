712a59d412b75b0219be0ca10b6e0171674a95c5
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-26 11:55:11 +0200

    Removed unused property and added required parameters

--- ClientServicing/Main/Models/Debicheck/CheckStatusResponse.cs
+++ ClientServicing/Main/Models/Debicheck/CheckStatusResponse.cs
@@ -1,12 +1,28 @@
-﻿using System.Collections.Generic;
-
-namespace ClientServicing.Main.Models.Debicheck
+﻿namespace ClientServicing.Main.Models.Debicheck
 {
     public class CheckStatusResponse
     {
         public bool success { get; set; }
         public string? message { get; set; }
-        public string? error { get; set; }
-        public List<CheckStatusRequest>? result { get; set; }
+        public List<CheckStatusResponseResult>? result { get; set; }
+    }
+    public class CheckStatusResponseResult {
+        public bool success { get; set; }
+        public List<CheckStatusResponseData>? data { get; set; }
+        public string? message { get; set; }
+    }
+    public class CheckStatusResponseData
+    {
+        public int amount { get; set; }
+        public bool ifaBusinessFeeIncluded { get; set; }
+        public bool success { get; set; }
+        public string? message { get; set; }
+        public string? status { get; set; }
+        public string? payerIdentityNumber { get; set; }
+        public string? payerMobileTelephoneNumber { get; set; }
+        public string? policyNumber { get; set; }
+        public DateTime createdAt { get; set; }
+        public string? mandateType { get; set; }
+
     }
 }

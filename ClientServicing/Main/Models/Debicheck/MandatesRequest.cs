59e39c1763db3ec2692e254d8d03be75a53577f5
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-26 12:04:16 +0200

    Added nullable to properties that are nullable

--- ClientServicing/Main/Models/Debicheck/MandatesRequest.cs
+++ ClientServicing/Main/Models/Debicheck/MandatesRequest.cs
@@ -8,10 +8,10 @@ namespace ClientServicing.Main.Models.Debicheck
 {
     public class MandatesRequest
     {
-        public required string policyNumber { get; set; }
-        public bool? existingClient { get; set; }
+        public required string? policyNumber { get; set; }
+        public bool existingClient { get; set; }
         public string? payerMobileTelephoneNumber { get; set; }
-        public int? sourceSystemId { get; set; }
+        public int sourceSystemId { get; set; }
         public string? agentCode { get; set; }
         public string? agentName { get; set; }
         public required int transactionType { get; set; }

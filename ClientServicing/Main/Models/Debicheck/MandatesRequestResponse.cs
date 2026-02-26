5bef990239ea52cea67fb3054abd2ac64f06c4b4
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-26 12:22:40 +0200

    Modified the class removed unused property, introduced a propery that is not list data collection list of MandatesRequestResponseResult this model class has defined propety.

--- ClientServicing/Main/Models/Debicheck/MandatesRequestResponse.cs
+++ ClientServicing/Main/Models/Debicheck/MandatesRequestResponse.cs
@@ -7,7 +7,12 @@ namespace ClientServicing.Main.Models.Debicheck
     {
         public bool success { get; set; }
         public bool? diderror { get; set; }
-        public ExecutionOutcome? responseMessage { get; set; }
-        public List<MandatesRequest>? result { get; set; }
+        public List<MandatesRequestResponseResult>? result { get; set; }
+    }
+    public class MandatesRequestResponseResult {
+        public bool success { get; set; }
+        public bool didError { get; set; }
+        public string message { get; set; }
+        public string? data { get; set; } //is return as null we need to find the data type of the property
     }
 }

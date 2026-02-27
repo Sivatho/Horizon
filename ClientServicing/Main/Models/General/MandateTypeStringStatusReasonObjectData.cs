987d87ea53fb25657dc1df7e60dd2b5c69b12a5a
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-02-27 09:44:56 +0200

    Added reusable object

---
+++ ClientServicing/Main/Models/General/MandateTypeStringStatusReasonObjectData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.General
+{
+    public class MandateTypeStringStatusReasonObjectData
+    {
+        public string mandateType { get; set; }
+        public List<statusReason> listOfStatusReason { get; set; }
+    }
+    public class statusReason {
+        public string message { get; set; }
+        public string policyNumber { get; set; }
+    }
+}
---
+++ ClientServicing/Main/Models/General/SuccessBoolMessageStringDataObjectResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.General
+{
+    public class SuccessBoolMessageStringDataObjectResult
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+        public MandateTypeStringStatusReasonObjectData data { get; set; }
+    }
+
+}

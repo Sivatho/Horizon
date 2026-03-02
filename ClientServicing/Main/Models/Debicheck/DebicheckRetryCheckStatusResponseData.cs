d84a27f3fcab1524758f3367a7f08e2f5039edce
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-03-02 14:44:52 +0200

    Created new model class

---
+++ ClientServicing/Main/Models/Debicheck/DebicheckRetryCheckStatusResponseData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.Debicheck
+{
+    public class DebicheckRetryCheckStatusResponseData
+    {
+        public bool success { get; set; }
+        public bool didError { get; set; }
+        public string message { get; set; }
+        public DebitCheckRetryStatusResponseDataModel data { get; set; }
+    }
+    public class DebitCheckRetryStatusResponseDataModel {
+        public string mandateType { get; set; }
+        public string? statusReason { get; set; }
+    }
+}

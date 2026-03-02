c90a2c4c60393643736374f4b5c9aac76c26f42d
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-03-02 11:59:20 +0200

    Added new Model

--- ClientServicing/Main/Models/Debicheck/DebicheckRetryCheckStatusResponse.cs
+++ ClientServicing/Main/Models/Debicheck/DebicheckRetryCheckStatusResponse.cs
@@ -1,12 +1,17 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace ClientServicing.Main.Models.Debicheck
-//{
-//    internal class CheckStatusRequest
-//    {
-//    }
-//}
+namespace ClientServicing.Main.Models.Debicheck
+{
+    public class DebicheckRetryCheckStatusResponse
+    {
+        public string policyNumber { get; set; }
+        public string debiCheckStatus { get; set; }
+        public int retryCount { get; set; }
+        public bool retryAllowed { get; set; }
+        public DateTime latestMandateCreatedAt { get; set; }
+    }
+}

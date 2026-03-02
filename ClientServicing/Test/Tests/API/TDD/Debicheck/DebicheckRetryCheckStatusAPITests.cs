b328d87a155fe3490c2c7fce54ffea312cba5f0e
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-03-02 11:59:58 +0200

    Added a new test method

---
+++ ClientServicing/Test/Tests/API/TDD/Debicheck/DebicheckRetryCheckStatusAPITests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ClientServicing.Main.AbstractComponents.API.ValidationMethods.Debicheck;
+using ClientServicing.Main.AbstractComponents.API.ValidationMethods.JsonValidation;
+using ClientServicing.Main.Controller;
+using ClientServicing.Main.DataAccess.Interface;
+using ClientServicing.Main.Models.Debicheck;
+using ClientServicing.Main.Resources.Helper;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Schema;
+
+namespace ClientServicing.Test.Tests.API.TDD.Debicheck
+{
+    [TestFixture]
+    public class DebicheckRetryCheckStatusAPITests : DebicheckRetryCheckStatusValidationMethods
+    {
+        DebicheckAPIClient? debicheckAPIClient = null!;
+        UtilitiesHelper utilitiesHelper = new();
+        private IDataAccess _dataAccess = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            debicheckAPIClient = new DebicheckAPIClient(GlobalTestInfrastructureSetup.SharedRestLibrary);
+            _dataAccess = GlobalTestInfrastructureSetup.ServiceProvider.GetRequiredService<IDataAccess>();
+        }
+        [Test]
+        public async Task Given_DebicheckRetryCheckStatusPayloadIsValid_When_DebicheckRetryCheckStatusAsync_Then()
+        {
+            //Arrange
+            var request = JsonSerializer.Deserialize<DebicheckRetryCheckStatusRequest>(
+                utilitiesHelper.ReadTestDataJson
+                ("Debicheck\\Data", "DebicheckRetryCheckStatusPayloadIsValid.json"))!;
+            ValidateDebicheckRetryCheckStatusRequestIsNotNullOrEmpty(request);
+            //Act
+            var response = await debicheckAPIClient!.DebicheckRetryCheckStatusAsync(request);
+            var debicheckRetryCheckStatusResponse = PopulateDebicheckRetryCheckStatusResponse(response);
+            var schema = ResponseSchemasEnvelope.DebicheckRetryCheckStatusSchema();
+            //Assert
+            ValidationAssertionHeading();
+            ValidateResponseStatusCode(response, HttpStatusCode.OK);
+            ValidateResponseHeadersAreValid(response);
+            ValidateResponsePropertyNameIsValidAndDataTypesIsValid(response, schema);
+            ValidateResponseShouldMatchSchema(response, schema);
+            ValidateDebicheckRetryCheckStatusResponseIsNotNullOrEmpty(debicheckRetryCheckStatusResponse);
+            
+        }
+    }
+}

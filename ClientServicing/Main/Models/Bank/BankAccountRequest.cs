6aadb05089bcd88d4ddc473a2ab18ab4c2656e00
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-03-05 14:26:57 +0200

    Added new model

---
+++ ClientServicing/Main/Models/Bank/BankAccountRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServicing.Main.Models.Bank
+{
+    public class BankAccountRequest
+    {
+        public int bankAccountID { get; set; }
+        public List<int> bankAccountList { get; set; }
+    }
+}

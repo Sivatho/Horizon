3b85125f8e5615733215b1d4fe5be4a6bb4610cc
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-03-05 14:08:05 +0200

    Removed IDisposal interface and implementation of Dispose method cleaned up ValidateAccountNumberUsageLimitAsync method execution

--- ClientServicing/Main/Controller/BankAPIClient.cs
+++ ClientServicing/Main/Controller/BankAPIClient.cs
@@ -7,7 +7,7 @@ using static ClientServicing.Main.Resources.EndPoints.Bank.BankAPIEndPoints;
 
 namespace ClientServicing.Main.Controller
 {
-    public class BankAPIClient : IBank, IDisposable
+    public class BankAPIClient : IBank
     {
         readonly RestClient restClient;
         readonly UtilitiesHelper utilitiesHelper = new UtilitiesHelper();
@@ -51,11 +51,16 @@ namespace ClientServicing.Main.Controller
             // Arrange
             var url = BankAPIEndPoints.GetEndPoint(EndPoints.FetchBank);
             Method method = Method.Post;
+            IDictionary<string, string>? headers = new Dictionary<string, string>
+            {
+                { "Accept", "*/*" }
+            };
             var request = ApiRequestAndResponseHelper.BuildRequest(
                 url,
                 method,
                 payload,
-                out var stopwatch);
+                out var stopwatch,
+                headers);
 
             // Act
             var response = await ApiRequestAndResponseHelper.ExecuteAsync(restClient, request, stopwatch);
@@ -66,10 +71,36 @@ namespace ClientServicing.Main.Controller
             //Arrange
             var url = BankAPIEndPoints.GetEndPoint(EndPoints.GetBankingDetailHistory);
             Method method = Method.Get;
-            IDictionary<string, string>? headers = null;
+            IDictionary<string, string>? headers = new Dictionary<string, string>
+            {
+                { "Accept", "*/*" }
+            };
             IDictionary<string, int>? urlSegment = new Dictionary<string, int>
             {
                 ["policyNo"] = policyNo
+            };
+            var request = ApiRequestAndResponseHelper.BuildRequest<object?>(
+                url,
+                method,
+                null,
+                out var stopwatch,
+                headers,
+                null,
+                urlSegment);
+
+            // Act
+            var response = await ApiRequestAndResponseHelper.ExecuteAsync(restClient, request, stopwatch);
+            return response;
+        }
+        public async Task<RestResponse> ValidateAccountNumberUsageLimitAsync(string accountNumber)
+        {       
+            //Arrange
+            var url = BankAPIEndPoints.GetEndPoint(EndPoints.ValidateAccountNumberUsageLimit);
+            Method method = Method.Get;
+            IDictionary<string, string>? headers = null;
+            IDictionary<string, int>? urlSegment = new Dictionary<string, int>
+            {
+                ["accountNumber"] = int.Parse(accountNumber)
             };
             var request = ApiRequestAndResponseHelper.BuildRequest<object?>(
                 url,
@@ -83,37 +114,6 @@ namespace ClientServicing.Main.Controller
             // Act
             var response = await ApiRequestAndResponseHelper.ExecuteAsync(restClient, request, stopwatch);
             return response;
-        }
-        public async Task<RestResponse> ValidateAccountNumberUsageLimitAsync(string accountNumber)
-        {
-            try
-            {
-                //Arrange
-                var request = new RestRequest(BankAPIEndPoints.GetEndPoint(EndPoints.ValidateAccountNumberUsageLimit), Method.Get);
-                request.AddUrlSegment("accountNumber", accountNumber);
-
-                //Act
-                var response = await restClient.ExecuteAsync(request);
-                utilitiesHelper.LogRequestAndResponse(request, response);
-
-                //Assert
-                if (!response.IsSuccessful)
-                {
-                    TestContext.Out.WriteLine($"ValidateAccountNumberUsageLimit > Response failed. Status:" +
-                        $" {response.StatusCode}," +
-                        $" {response.ErrorMessage}");
-                }
-                return response;
-            }
-            catch(Exception ex) {
-                TestContext.Out.WriteLine($"\tValidateAccountNumberUsageLimit > Exception occurred: {ex.Message}");
-                TestContext.Out.WriteLine($"\tValidateAccountNumberUsageLimit > Stack Trace: {ex.StackTrace}");
-                return new RestResponse
-                {
-                    StatusCode = System.Net.HttpStatusCode.InternalServerError,
-                    ErrorMessage = ex.Message
-                };
-            }
         }
         public async Task<RestResponse> ValidateBankAccountAsync<T>(T payload) where T : class
         {
@@ -158,11 +158,5 @@ namespace ClientServicing.Main.Controller
             var response = await ApiRequestAndResponseHelper.ExecuteAsync(restClient, request, stopwatch);
             return response;
         }
-        public void Dispose()
-        {
-            restClient?.Dispose();
-            GC.SuppressFinalize(this);
-        }
-
     }
 }

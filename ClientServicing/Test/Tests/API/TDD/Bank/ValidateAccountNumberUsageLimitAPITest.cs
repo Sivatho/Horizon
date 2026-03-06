9066a899483cbe29a97c3e206addf11db3f27348
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-03-06 10:12:24 +0200

    Modified Test Class Integrated shared REST client and centralized request builder (ApiRequestAndResponseHelper) Added consistent header setup and URL segment handling Improved exception handling and logging for validation methods Ensured all API calls return RestResponse asynchronously

--- ClientServicing/Test/Tests/API/TDD/Bank/ValidateAccountNumberUsageLimitAPITest.cs
+++ ClientServicing/Test/Tests/API/TDD/Bank/ValidateAccountNumberUsageLimitAPITest.cs
@@ -1,56 +1,48 @@
 ﻿using System.Net;
 using System.Text.Json;
 using ClientServicing.Main.AbstractComponents.API.ValidationMethods.Bank;
+using ClientServicing.Main.AbstractComponents.API.ValidationMethods.JsonValidation;
 using ClientServicing.Main.Controller;
+using ClientServicing.Main.DataAccess.Interface;
 using ClientServicing.Main.Models.Bank;
-using ClientServicing.Main.Models.General;
+using ClientServicing.Main.Resources.Helper;
+using Microsoft.Extensions.DependencyInjection;
 using RestSharp;
 
 namespace ClientServicing.Test.Tests.API.TDD.Bank
 {
     public class ValidateAccountNumberUsageLimitAPITest : ValidateAccountNumberUsageLimitValidationMethods
     {
-        BankAPIClient bankAPIClient = new();
+        BankAPIClient bankAPIClient = null;
+        UtilitiesHelper utilitiesHelper = new();
+        private IDataAccess _dataAccess = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            bankAPIClient = new BankAPIClient(GlobalTestInfrastructureSetup.SharedRestLibrary);
+            _dataAccess = GlobalTestInfrastructureSetup.ServiceProvider.GetRequiredService<IDataAccess>();
+        }
         [Test]
         public async Task Given_AccountNumberUsageLimitIsValid_When_ValidateAccountNumberUsageLimitAsync_Then_ValidateResponseStatusCodeOK_And_PropertyNameIsValid_And_DataTypesIsValid_And_IsNotNullOrEmpty()
         {
             //Arrange
-            string accountNumber = "1042631522";
+            var request = JsonSerializer.Deserialize<List<EntityBankAccountDetails>>(
+                utilitiesHelper.ReadTestDataJson("Bank\\Data", "EntityBankAccountDetailsIsValid.json"));
+            int randomIndex = new Random().Next(0, request!.Count);
+            string accountNumber = request![randomIndex].BankAccNo;
 
             //Act
             var response = await bankAPIClient.ValidateAccountNumberUsageLimitAsync(accountNumber);
-            var validateAccountNumberUsageLimitResponse = populateValidateAccountNumberUsageLimitResponse(response);
-
+            var validateAccountNumberUsageLimitResponse = PopulateValidateAccountNumberUsageLimitResponse(response);
+            var schema = PolicySchemas.PolicyStatusBody();
             //Assert
             ValidationAssertionHeading();
             ValidateResponseStatusCode(response, HttpStatusCode.OK);
-            ValidateResponsePropertyNameIsValid_And_DataTypesIsValid(response);
+            ValidateResponseHeadersAreValid(response);
+            ValidateResponsePropertyNameIsValidAndDataTypesIsValid(response, schema);
+            ValidateResponseShouldMatchSchema(response, schema);
             ValidateValidateAccountNumberUsageLimitResponseDataIsNotNullOrEmpty(validateAccountNumberUsageLimitResponse);
         }
-        private ValidateAccountNumberUsageLimitResponse populateValidateAccountNumberUsageLimitResponse(RestResponse response)
-        {
-            using JsonDocument document = JsonDocument.Parse(response.Content);
-            ValidateAccountNumberUsageLimitResponse validateAccountNumberUsageLimitResponse = new();
-            foreach (var property in document.RootElement.EnumerateObject())
-            {
-                switch (property.Name.ToLower())
-                {
-                    case "succeeded":
-                        validateAccountNumberUsageLimitResponse.success = property.Value.GetBoolean();
-                        break;
-                    case "message":
-                        validateAccountNumberUsageLimitResponse.message = property.Value.GetString();
-                        break;
-                    case "totalPolicies":
-                        validateAccountNumberUsageLimitResponse.totalPolicies = property.Value.GetInt32();
-                        break;
-                    case "limitExceeded":
-                        validateAccountNumberUsageLimitResponse.limitExceeded = property.Value.GetBoolean();
-                        break;
-                }
-            }
-            return validateAccountNumberUsageLimitResponse;
-        }
-
     }
 }

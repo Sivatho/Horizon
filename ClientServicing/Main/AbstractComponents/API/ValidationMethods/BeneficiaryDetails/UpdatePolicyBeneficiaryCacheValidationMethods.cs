d22a8fa425b9f464ed8ba49b6629605f0f2e33b5
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-03-10 18:26:31 +0200

    add UpdatePolicyBeneficiaryCache response parsing and core validations
    Implement UpdatePolicyBeneficiaryCacheValidationMethods with:
    
    PopulateUpdatePolicyBenefitciaryResponse to map JSON (succeeded, message, error(s), data) to model
    ValidateUpdatePolicyBenefitciaryCacheResponseDataIsNotNullOrEmpty to assert non-null response, boolean fields, and optional strings
    
    Utilize UtilitiesHelper for safe JSON extraction and log unknown properties
    Add result summary via DocumentTemplate.DisplayBody
    Leave abstract method overrides for field and property/type validation as NotImplemented (to be completed)

--- ClientServicing/Main/AbstractComponents/API/ValidationMethods/BeneficiaryDetails/UpdatePolicyBeneficiaryCacheValidationMethods.cs
+++ ClientServicing/Main/AbstractComponents/API/ValidationMethods/BeneficiaryDetails/UpdatePolicyBeneficiaryCacheValidationMethods.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 using ClientServicing.Main.AbstractComponents.API.IValidationMethods.BeneficiaryDetails;
 using ClientServicing.Main.Models.BeneficiaryDetails;
 using ClientServicing.Main.Resources.Helper;
@@ -13,47 +8,49 @@ namespace ClientServicing.Main.AbstractComponents.API.ValidationMethods.Benefici
 {
     public class UpdatePolicyBeneficiaryCacheValidationMethods : AbstractValidationMethods, IUpdatePolicyBeneficiaryCacheValidationMethods
     {
+        UtilitiesHelper utilitiesHelper = new();
+        public void ValidateUpdatePolicyBenefitciaryCacheResponseDataIsNotNullOrEmpty(UpdatePolicyBenefitciaryResponse updatePolicyBenefitciaryCacheResponse)
+        {
+            Assert.Multiple(() =>{
+                Assert.That(updatePolicyBenefitciaryCacheResponse,              Is.Not.Null.Or.Empty,           "Validated: UpdatePolicyBeneficiaryCacheResponse: Response should not be null or empty");
+                Assert.That(updatePolicyBenefitciaryCacheResponse.succeeded,    Is.True.Or.False,               "Validated: UpdatePolicyBeneficiaryCacheResponse.Succeeded property should not be null");
+                Assert.That(updatePolicyBenefitciaryCacheResponse.data,         Is.True.Or.False,               "Validated: UpdatePolicyBeneficiaryCacheResponse.Data property should not be null");
+                Assert.That(updatePolicyBenefitciaryCacheResponse.message,      Is.Null.Or.TypeOf<string>(),    "Validated: UpdatePolicyBeneficiaryCacheResponse.Message property should not be null or empty");
+                Assert.That(updatePolicyBenefitciaryCacheResponse.errors,       Is.Null.Or.TypeOf<string>(),    "Validated: UpdatePolicyBeneficiaryCacheResponse.Errors property should not be null or empty");
+            });
+            DocumentTemplate.DisplayBody("Validated: UpdatePolicyBeneficiaryCacheResponse: Is Not Null, Is True Or False, Is Null Or Type Of String , Is Not Less Than Or Equal To 0");
+        }
+        public UpdatePolicyBenefitciaryResponse PopulateUpdatePolicyBenefitciaryResponse(RestResponse restResponse)
+        {
+            var updatePolicyBenefitCacheResponse = new UpdatePolicyBenefitciaryResponse();
+            using JsonDocument doc = JsonDocument.Parse(restResponse.Content!);
+
+            foreach (var property in doc.RootElement.EnumerateObject())
+            {
+                switch (property.Name)
+                {
+                    case "succeeded":
+                        updatePolicyBenefitCacheResponse.succeeded = (bool)utilitiesHelper.ReadBooleanNullable(property.Value); break;
+                    case "message":
+                        updatePolicyBenefitCacheResponse.message = utilitiesHelper.ReadStringNullable(property.Value); break;
+                    case "error":
+                        updatePolicyBenefitCacheResponse.errors = utilitiesHelper.ReadStringNullable(property.Value); break;
+                    case "data":
+                        updatePolicyBenefitCacheResponse.data = (bool)utilitiesHelper.ReadBooleanNullable(property.Value); break;
+                    default:
+                        TestContext.Out.WriteLine($"Unknown property: {property.Name}");
+                        break;
+                }
+            }
+            return updatePolicyBenefitCacheResponse;
+        }
         public override void ValidateResponseFieldParametersIsValid(RestResponse restResponse)
         {
             throw new NotImplementedException();
         }
-
         public override void ValidateResponsePropertyNameIsValid_And_DataTypesIsValid(RestResponse restResponse)
         {
-            var rules = new List<JsonValidationRule> {
-                new JsonValidationRule {
-                    PropertyName = "succeeded",
-                    AllowedKinds = new[] {
-                        JsonValueKind.True, JsonValueKind.False
-                    }
-                },
-                new JsonValidationRule {
-                    PropertyName = "message",
-                    AllowedKinds = new[] {
-                        JsonValueKind.String, JsonValueKind.Null
-                    }
-                },
-                new JsonValidationRule {
-                    PropertyName = "errors",
-                    AllowedKinds = new[] {
-                        JsonValueKind.String, JsonValueKind.Null
-                    }
-                },
-                new JsonValidationRule {
-                    PropertyName = "data",
-                    AllowedKinds = new[] {
-                        JsonValueKind.True, JsonValueKind.False, JsonValueKind.Null
-                    }
-                }
-            };
-            using var jsonDoc = JsonDocument.Parse(restResponse.Content);
-            JsonValidationRule.ValidateJson(jsonDoc.RootElement, rules);
-            TestContext.Out.WriteLine("Validated: Response Property Names are valid and Data Types are valid.");
-        }
-        public void ValidateUpdatePolicyBenefitciaryCacheResponseDataIsNotNullOrEmpty(UpdatePolicyBenefitciaryResponse updatePolicyBenefitciaryCacheResponse)
-        {
-            Assert.That(updatePolicyBenefitciaryCacheResponse, Is.Not.Null.Or.Empty, "UpdatePolicyBeneficiaryCacheResponse: Response should not be null or empty");
-            TestContext.Out.WriteLine("Validated: <UpdatePolicyBeneficiaryCacheResponse> is not null or empty.");
+            throw new NotImplementedException();
         }
     }
 }

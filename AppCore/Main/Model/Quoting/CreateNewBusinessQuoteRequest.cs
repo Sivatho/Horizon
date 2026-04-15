79229767fef3b3f7565ff44d33a972116bf2b509
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-04-15 13:03:19 +0200

    Updated Data type

--- AppCore/Main/Model/Quoting/CreateNewBusinessQuoteRequest.cs
+++ AppCore/Main/Model/Quoting/CreateNewBusinessQuoteRequest.cs
@@ -2,11 +2,11 @@
 {
     public class CreateNewBusinessQuoteRequest
     {
-        public int schemaCd { get; set; }
-        public int partnerCd { get; set; }
-        public int planCd { get; set; }
-        public string user { get; set; }
-        public string effectiveDate { get; set; }
-        public int quoteTypeCd { get; set; }
+        public int? schemaCd { get; set; }
+        public int? partnerCd { get; set; }
+        public int? planCd { get; set; }
+        public string? user { get; set; }
+        public DateTime? effectiveDate { get; set; }
+        public int? quoteTypeCd { get; set; }
     }
 }

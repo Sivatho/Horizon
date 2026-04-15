fd2b52afe25a825e48a108a386401c93e20b9ee9
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-04-15 13:06:54 +0200

    Modified class properties to nullable

--- AppCore/Main/Model/Quoting/GetQuoteRulesetRequest.cs
+++ AppCore/Main/Model/Quoting/GetQuoteRulesetRequest.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppCore.Main.Model.Quoting
+﻿namespace AppCore.Main.Model.Quoting
 {
     public class GetQuoteRulesetRequest
     {
         public CreateNewBusinessQuoteRequest quoteRequest { get; set; }
-        public int planCategoryCd { get; set; }
-        public int brokerEntityNo { get; set; }
+        public int? planCategoryCd { get; set; }
+        public int? brokerEntityNo { get; set; }
     }
 }
